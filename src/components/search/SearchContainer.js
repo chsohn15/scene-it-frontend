f8ed23d0466ccb832d2d9ea64450f97fc3e45478
chsohn15 <chsohn15@gmail.com>
2024-12-26 23:23:35 -0500

    add styled components, change fetch url

--- src/components/search/SearchContainer.js
+++ src/components/search/SearchContainer.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import SearchCard from "./SearchCard.js";
+import styled from 'styled-components';
 
 function SearchContainer() {
   const [productions, setProductions] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:3000/productions")
+    fetch("https://scene-it-api-52ad0b1f04cb.herokuapp.com/productions")
       .then((res) => {
         if (!res.ok) {
           console.log('There was an error in fetching data');
@@ -20,9 +21,15 @@ function SearchContainer() {
       });
   }, []);
 
+  const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`;
+
   return (
     <div class="main">
-      <h1>Browse Films and Movies</h1>
+      <Title>Browse Films and Movies</Title>
       <div class="cards-container">
       {productions.map((production) => (
         <SearchCard production={production} />
